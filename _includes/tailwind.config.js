abfe51f2a00687d18238356de9990348ff257c09
Iiro Krankka <iiro.krankka@gmail.com>
2021-05-06 23:05:33 +0300

    Yay for dark mode :-)

--- _includes/tailwind.config.js
+++ _includes/tailwind.config.js
@@ -1,18 +1,86 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-    defaultLineHeights: true,
-    standardFontWeights: true,
-  },
+  darkMode: "media",
   purge: ["_site/**/*.html"],
   theme: {
     extend: {
+      colors: {
+        'gray-725': '#313B4B',
+        'gray-750': '#2B3544',
+        'gray-775': '#252F3E',
+        'gray-825': '#1C2533',
+        'gray-850': '#18212F',
+        'gray-875': '#151C2B'
+      },
+      typography: (theme) => ({
+        light: {
+          css: [
+            {
+              color: theme('colors.gray.300'),
+              '[class~="lead"]': {
+                color: theme('colors.gray.300'),
+              },
+              a: {
+                color: theme('colors.white'),
+              },
+              strong: {
+                color: theme('colors.white'),
+              },
+              'ol > li::before': {
+                color: theme('colors.gray.400'),
+              },
+              'ul > li::before': {
+                backgroundColor: theme('colors.gray.600'),
+              },
+              hr: {
+                borderColor: theme('colors.gray.200'),
+              },
+              blockquote: {
+                color: theme('colors.gray.200'),
+                borderLeftColor: theme('colors.gray.600'),
+              },
+              h1: {
+                color: theme('colors.white'),
+              },
+              h2: {
+                color: theme('colors.white'),
+              },
+              h3: {
+                color: theme('colors.white'),
+              },
+              h4: {
+                color: theme('colors.white'),
+              },
+              'figure figcaption': {
+                color: theme('colors.gray.400'),
+              },
+              code: {
+                color: theme('colors.white'),
+              },
+              'a code': {
+                color: theme('colors.white'),
+              },
+              pre: {
+                color: theme('colors.gray.200'),
+                backgroundColor: theme('colors.gray.800'),
+              },
+              thead: {
+                color: theme('colors.white'),
+                borderBottomColor: theme('colors.gray.400'),
+              },
+              'tbody tr': {
+                borderBottomColor: theme('colors.gray.600'),
+              }
+            },
+          ],
+        },
+      }),
     },
   },
-  variants: {},
+  variants: {
+    typography: ['dark']
+  },
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
